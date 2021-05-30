b2bdba579695a31204c6c1f479d0a7862c7372b9
Dmitrii Tikhomirov <chani.liet@gmail.com>
2021-05-29 20:10:15 -0400

    complete WebXR externs and elemental2 classes implementation

--- java/org/treblereel/gwt/elemental2/webxr/webxr.js
+++ java/org/treblereel/gwt/elemental2/webxr/webxr.js
@@ -26,35 +26,718 @@
 Navigator.prototype.xr;
 
 /**
+ * @type {boolean}
+ */
+WebGLContextAttributes.prototype.xrCompatible;
+
+/**
+ * @record
+ */
+var XRSessionInit = function () { };
+
+/**
+  * @type {Array<string>}
+  */
+XRSessionInit.prototype.requiredFeatures;
+
+/**
+  * @type {Array<string>}
+  */
+XRSessionInit.prototype.optionalFeatures;
+
+/**
  * @interface
  * @extends {EventTarget}
  */
-var XRSystem = function() {};
+var XRSystem = function () { };
 
 /**
  * @param {XRSessionMode} mode
- * @return  {Promise<boolean>}
+ * @return {Promise<boolean>}
  */
-XRSystem.prototype.isSessionSupported = function(mode){};
+XRSystem.prototype.isSessionSupported = function (mode) { };
 
 /**
  * @param {XRSessionMode} mode
- * @return  {XRSession}
+ * @param {XRSessionInit=} opt_XRSessionInit
+ * @return {Promise<XRSession>}
  */
-XRSystem.prototype.requestSession = function(mode){};
+XRSystem.prototype.requestSession = function (mode, opt_XRSessionInit) { };
 
+/** @type {?function (Event)} */ XRSystem.prototype.ondevicechange;
+
+/**
+ * @record
+ * @extends {EventInit}
+ */
+var XRSessionEventInit = function() {};
+
+/**
+ * @type {XRSession}
+ */
+XRSessionEventInit.prototype.session;
+
+/**
+ * @constructor
+ * @extends {Event}
+ * @param {string} type
+ * @param {XRSessionEventInit=} eventInitDict
+ */
+var XRSessionEvent = function (type, eventInitDict) { };
+
+/**
+ * @const
+ * @type {XRSession}
+ */
+XRSessionEvent.prototype.session;
+
+/**
+ * @record
+ * @extends {EventInit}
+ */
+var XRInputSourceEventInit = function() {};
+
+/**
+ * @type {XRFrame}
+ */
+XRInputSourceEventInit.prototype.frame;
+
+/**
+ * @type {XRInputSource}
+ */
+XRInputSourceEventInit.prototype.inputSource;
+
+/**
+ * @constructor
+ * @extends {Event}
+ * @param {string} type
+ * @param {XRInputSourceEventInit} eventInitDict
+ */
+var XRInputSourceEvent = function (type, eventInitDict) { };
+
+/**
+ * @const
+ * @type {XRInputSource}
+ */
+XRInputSourceEvent.prototype.inputSource;
+
+/**
+ * @const
+ * @type {XRFrame}
+ */
+XRInputSourceEvent.prototype.frame;
+
+/**
+ * @record
+ * @extends {EventInit}
+ */
+var XRInputSourcesChangeEventInit = function() {};
+
+/**
+ * @type {XRSession}
+ */
+XRInputSourcesChangeEventInit.prototype.session;
+
+/**
+ * @type {Array<XRInputSource>}
+ */
+XRInputSourcesChangeEventInit.prototype.added;
+
+/**
+ * @type {Array<XRInputSource>}
+ */
+XRInputSourcesChangeEventInit.prototype.removed;
+
+/**
+ * @constructor
+ * @extends {Event}
+ * @param {string} type
+ * @param {XRInputSourcesChangeEventInit} eventInitDict
+ */
+var XRInputSourcesChangeEvent = function (type, eventInitDict) { };
+
+/**
+ * @const
+ * @type {Array<XRInputSource>}
+ */
+XRInputSourcesChangeEvent.prototype.added;
+
+/**
+ * @const
+ * @type {Array<XRInputSource>}
+ */
+XRInputSourcesChangeEvent.prototype.removed;
+
+
+/**
+ * @record
+ * @extends {EventInit}
+ */
+var XRReferenceSpaceEventInit = function() {};
+
+/**
+ * @type {XRRigidTransform?}
+ */
+XRReferenceSpaceEventInit.prototype.transform;
+
+/**
+ * @type {XRReferenceSpace}
+ */
+XRReferenceSpaceEventInit.prototype.referenceSpace;
+
+/**
+ * @constructor
+ * @extends {Event}
+ * @param {string} type
+ * @param {XRReferenceSpaceEventInit} eventInitDict
+ */
+var XRReferenceSpaceEvent = function (type, eventInitDict) { };
+
+/**
+ * @const
+ * @type {XRReferenceSpace}
+ */
+XRReferenceSpaceEvent.prototype.referenceSpace;
+
+/**
+ * @const
+ * @type {XRRigidTransform?}
+ */
+XRReferenceSpaceEvent.prototype.transform;
 
 /**
  * @enum
  */
 var XRSessionMode = {
-  IMMERSIVEAR: 'immersive-ar', 
-  IMMERSIVEVR: 'immersive-vr', 
+  IMMERSIVEAR: 'immersive-ar',
+  IMMERSIVEVR: 'immersive-vr',
   INLINE: 'inline'
+};
+
+/**
+ * @enum
+ */
+var XRVisibilityState = {
+  VISIBLE: 'visible',
+  VISIBLEBLURRED: 'visible-blurred',
+  HIDDEN: 'hidden'
+};
+
+/**
+* @interface
+* @extends {EventTarget}
+*/
+var XRSession = function () { };
+
+/**
+ * @const
+ * @type {XRVisibilityState}
+ */
+XRSession.prototype.visibilityState;
+
+/**
+ * @const
+ * @type {XRInputSourceArray}
+ */
+XRSession.prototype.inputSources;
+
+/**
+ * @const
+ * @type {XRRenderState}
+ */
+XRSession.prototype.renderState;
+
+/**
+ * @param {XRRenderStateInit=} state
+ * @return {undefined}
+ */
+XRSession.prototype.updateRenderState = function (state) { };
+
+/**
+ * @param {number} handle
+ * @return {undefined}
+ */
+XRSession.prototype.cancelAnimationFrame = function (handle) { };
+
+/**
+* @return {Promise<void>}
+*/
+XRSession.prototype.end = function () { };
+
+/** @type {?function (Event)} */ XRSession.prototype.onend;
+/** @type {?function (Event)} */ XRSession.prototype.oninputsourceschange;
+/** @type {?function (Event)} */ XRSession.prototype.onselect;
+/** @type {?function (Event)} */ XRSession.prototype.onselectstart;
+/** @type {?function (Event)} */ XRSession.prototype.onselectend;
+/** @type {?function (Event)} */ XRSession.prototype.onsqueeze;
+/** @type {?function (Event)} */ XRSession.prototype.onsqueezestart;
+/** @type {?function (Event)} */ XRSession.prototype.onsqueezeend;
+/** @type {?function (Event)} */ XRSession.prototype.onvisibilitychange;
+
+/**
+ * @typedef {function(number, XRFrame): undefined}
+ */
+var XRFrameRequestCallback;
+
+/**
+ * @param {!XRFrameRequestCallback} callback
+ * @return {number}
+ */
+XRSession.prototype.requestAnimationFrame = function (callback) { }
+
+/**
+ * @param {XRReferenceSpaceType} referenceSpaceType
+ * @return {Promise<XRReferenceSpace|XRBoundedReferenceSpace>}
+ * @throws {!NotSupportedError}
+ */
+XRSession.prototype.requestReferenceSpace = function (referenceSpaceType) { };
+
+/**
+ * @enum
+ */
+var XRReferenceSpaceType = {
+  BOUNDEDFLOOR: 'bounded-floor',
+  LOCAL: 'local',
+  LOCALFLOOR: 'local-floor',
+  UNBOUNDED: 'unbounded',
+  VIEWER: 'viewer'
+};
+
+/**
+ * @enum
+ */
+var XRHandedness = {
+  NONE: 'none',
+  LEFT: 'left',
+  RIGHT: 'right',
   };
 
- /**
+/**
+ * @enum
+ */
+var XRTargetRayMode = {
+  GAZE: 'gaze',
+  TRACKEDPOINTER: 'tracked-pointer',
+  SCREEN: 'screen',
+  };
+
+/**
+* @interface
+*/
+var XRInputSource = function () { };
+
+/**
+ * @const
+ * @type {XRSpace?}
+ */
+XRInputSource.prototype.gripSpace;
+
+/**
+ * @const
+ * @type {XRHandedness}
+ */
+XRInputSource.prototype.handedness;
+
+/**
+ * @const
+ * @type {Array<string>}
+ */
+XRInputSource.prototype.profiles;
+
+/**
+ * @const
+ * @type {XRTargetRayMode}
+ */
+XRInputSource.prototype.targetRayMode;
+
+/**
+ * @const
+ * @type {XRSpace}
+ */
+XRInputSource.prototype.targetRaySpace;
+
+/**
+* @interface
+ * @extends {Iterable<XRInputSource>}
+*/
+var XRInputSourceArray = function () { };
+
+/**
+ * @const {number}
+ */
+XRInputSourceArray.prototype.length;
+
+/**
+* @interface
+* @extends {EventTarget}
+*/
+var XRSpace = function () { };
+
+/**
+* @interface
+* @extends {XRSpace}
+*/
+var XRReferenceSpace = function () { };
+
+/**
+ * @param {XRRigidTransform}  originOffset
+ * @return {XRReferenceSpace}
+ */
+XRReferenceSpace.prototype.getOffsetReferenceSpace = function (originOffset) { };
+
+/** @type {?function (Event)} */ XRReferenceSpace.prototype.onreset;
+
+/**
+* @interface
+* @extends {XRReferenceSpace}
+*/
+var XRBoundedReferenceSpace = function () { };
+
+/**
+ * @const
+ * @type {DOMPointReadOnly}
+ */
+XRBoundedReferenceSpace.prototype.boundsGeometry;
+
+/**
+ * @record
+ * @struct
+ */
+var XRRenderStateInit = function () { };
+
+/**
+ * @type {XRWebGLLayer}
+ */
+XRRenderStateInit.prototype.baseLayer;
+
+/**
+ * @type {number}
+ */
+XRRenderStateInit.prototype.depthFar;
+
+/**
+ * @type {number}
+ */
+XRRenderStateInit.prototype.depthNear;
+
+/**
+ * @type {number}
+ */
+XRRenderStateInit.prototype.inlineVerticalFieldOfView;
+
+/**
+ * @type {Array<XRLayer>}
+ */
+XRRenderStateInit.prototype.layers;
+
+/**
+ * @constructor
+ * @extends {DOMError}
+ */
+function NotSupportedError() { }
+
+/**
+ * @interface
+ */
+var XRPose = function () { };
+
+/**
+ * @const
+ * @type {XRRigidTransform}
+ */
+XRPose.prototype.transform;
+
+/**
+ * @const
+ * @type {boolean}
+ */
+XRPose.prototype.emulatedPosition;
+
+/**
+ * @const
+ * @type {DOMPointReadOnly?}
+ */
+XRPose.prototype.linearVelocity;
+
+/**
+ * @const
+ * @type {DOMPointReadOnly?}
+ */
+XRPose.prototype.angularVelocity;
+
+/**
+ * @enum
+ */
+var XREye = {
+  NONE: 'none',
+  LEFT: 'left',
+  RIGHT: 'right',
+  };
+
+/**
+ * @interface
+ */
+var XRView = function () { };
+
+/**
+ * @const
+ * @type {XREye}
+ */
+XRView.prototype.eye;
+
+/**
+ * @const
+ * @type {Float32Array}
+ */
+XRView.prototype.transform;
+
+/**
+ * @const
+ * @type {XRRigidTransform}
+ */
+XRView.prototype.projectionMatrix;
+
+/**
+ * @const
+ * @type {number?}
+ */
+XRView.prototype.recommendedViewportScale;
+
+/**
+ * @param {number?} scale
+ * @return {undefined}
+ */
+XRView.prototype.requestViewportScale = function(scale) {};
+
+/**
+ * @interface
+ */
+var XRViewerPose = function () { };
+
+/**
+ * @const
+ * @type {Array<XRView>}
+ */
+XRViewerPose.prototype.views;
+
+/**
+ * @interface
+ */
+var XRViewport = function () { };
+
+/**
+ * @const
+ * @type {number}
+ */
+XRViewport.prototype.x;
+
+/**
+ * @const
+ * @type {number}
+ */
+XRViewport.prototype.y;
+
+/**
+ * @const
+ * @type {number}
+ */
+XRViewport.prototype.width;
+
+/**
+ * @const
+ * @type {number}
+ */
+XRViewport.prototype.height;
+
+/**
+* @interface
+*/
+var XRFrame = function () { };
+
+/**
+ * @const
+ * @type {XRSession}
+ */
+XRFrame.prototype.session;
+
+/**
+ * @param {XRSpace} space
+ * @param {XRSpace} baseSpace
+ * @return {XRPose}
+ */
+XRFrame.prototype.getPose = function (space, baseSpace) { };
+
+/**
+ * @param {XRReferenceSpace} referenceSpace
+ * @return {XRViewerPose}
+ */
+XRFrame.prototype.getViewerPose = function (referenceSpace) { };
+
+/**
  * @interface
  * @extends {EventTarget}
- */ 
- var XRSession = function() {};
+ */
+var XRLayer = function () { };
+
+/**
+ * @record
+ */
+var XRWebGLLayerInit = function () { };
+
+/** @type {boolean} */ XRWebGLLayerInit.prototype.antialias;
+/** @type {boolean} */ XRWebGLLayerInit.prototype.depth;
+/** @type {boolean} */ XRWebGLLayerInit.prototype.stencil;
+/** @type {boolean} */ XRWebGLLayerInit.prototype.alpha;
+/** @type {boolean} */ XRWebGLLayerInit.prototype.ignoreDepthValues;
+/** @type {number} */ XRWebGLLayerInit.prototype.framebufferScaleFactor;
+
+/**
+* @interface
+*/
+var XRRenderState = function () { };
+
+/**
+ * @const
+ * @type {XRWebGLLayer}
+ */
+XRRenderState.prototype.baseLayer;
+
+/**
+ * @const
+ * @type {number}
+ */
+XRRenderState.prototype.depthFar;
+
+/**
+ * @const
+ * @type {number}
+ */
+XRRenderState.prototype.depthNear;
+
+/**
+ * @const
+ * @type {number|null}
+ */
+XRRenderState.prototype.inlineVerticalFieldOfView;
+
+/**
+* @interface
+*/
+var XRViewport = function () { };
+
+/**
+ * @const
+ * @type {number}
+ */
+XRViewport.prototype.height;
+
+/**
+ * @const
+ * @type {number}
+ */
+XRViewport.prototype.width;
+
+/**
+ * @const
+ * @type {number}
+ */
+XRViewport.prototype.x;
+
+/**
+ * @const
+ * @type {number}
+ */
+XRViewport.prototype.y;
+
+/**
+ * @constructor
+ * @param {XRSession} session
+ * @param {WebGLRenderingContext} context
+ * @param {XRWebGLLayerInit=} layerInit
+ * @implements {XRLayer}
+ */
+var XRWebGLLayer = function (session, context, layerInit) { };
+
+/** @override */
+XRWebGLLayer.prototype.addEventListener = function(
+    type, listener, opt_options) {};
+
+/** @override */
+XRWebGLLayer.prototype.removeEventListener = function(
+    type, listener, opt_options) {};
+
+/**
+ * @const
+ * @type {boolean}
+ */
+XRWebGLLayer.prototype.antialias;
+
+/**
+ * @const
+ * @type {WebGLFramebuffer}
+ */
+XRWebGLLayer.prototype.framebuffer;
+
+/**
+ * @const
+ * @type {number}
+ */
+XRWebGLLayer.prototype.framebufferWidth;
+
+/**
+ * @const
+ * @type {number}
+ */
+XRWebGLLayer.prototype.framebufferHeight;
+
+/**
+ * @const
+ * @type {boolean}
+ */
+XRWebGLLayer.prototype.ignoreDepthValues;
+
+/**
+ * @return {XRViewport}
+ */
+XRWebGLLayer.prototype.getViewport = function () { };
+
+/**
+ * @static
+ * @return {number}
+ */
+XRWebGLLayer.prototype.getNativeFramebufferScaleFactor = function () { };
+
+/**
+ * @constructor
+ */
+var XRRigidTransform = function () { };
+
+/**
+ * @const
+ * @type {DOMPointReadOnly}
+ */
+XRRigidTransform.prototype.position;
+
+/**
+ * @const
+ * @type {DOMPointReadOnly}
+ */
+XRRigidTransform.prototype.orientation;
+
+/**
+ * @const
+ * @type {Float32Array}
+ */
+XRRigidTransform.prototype.matrix;
+
+/**
+ * @const
+ * @type {XRRigidTransform}
+ */
+XRRigidTransform.prototype.inverse;
+
+/**
+ * @return {Promise<void>}
+ */
+WebGLRenderingContext.prototype.makeXRCompatible = function () { };
+
