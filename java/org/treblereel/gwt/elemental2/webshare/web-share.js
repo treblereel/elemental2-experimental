b28f11f638c7d87d5a0b4d631d2ff3a3717393cd
Dmitrii Tikhomirov <treblereel@users.noreply.github.com>
2021-12-06 07:36:33 -0800

    added WebShare API (#11)

---
+++ java/org/treblereel/gwt/elemental2/webshare/web-share.js
@@ -0,0 +1,43 @@
+/**
+ * @externs
+ */
+
+
+
+/**
+ * @param {ShareData=} data
+ */
+Navigator.prototype.share = function(data) { };
+
+
+/**
+ * @record
+ */
+var ShareData = function () { };
+
+/**
+ * @type {Array<File>}
+ */
+ShareData.prototype.files;
+
+/**
+ * @type {string}
+ */
+ShareData.prototype.title;
+
+/**
+ * @type {string}
+ */
+ShareData.prototype.text;
+
+/**
+ * @type {string}
+ */
+ShareData.prototype.url;
+
+
+
+
+
+
+
